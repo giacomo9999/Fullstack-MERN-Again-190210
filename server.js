54d3c191f4bf5f05227f71f8e2be099204259004
James Gary <gmarchesi888@gmail.com>
2019-02-11 12:42:44 -0500

    Server runs and connects to MongoDB.

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const PORT = 4000;
+const cors = require("cors");
+const config = require("./DB");
+
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(() => {
+  console.log("Connected to database."),
+    err => {
+      console.log("Cannot connect to database.");
+    };
+});
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/test", (req, res) => res.send("Here I am at the test endpoint."));
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(PORT, () => {
+  console.log("Server running on the following port", PORT);
+});
