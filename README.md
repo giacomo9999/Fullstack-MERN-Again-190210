fee0e23627aadcbdb1b53aae90e0621e0b8389ca
James Gary <gmarchesi888@gmail.com>
2019-02-11 17:18:13 -0500

    Ready to deploy to Heroku.

--- README.md
+++ README.md
@@ -2,4 +2,9 @@ From here:
 
 https://appdividend.com/2018/05/30/react-axios-tutorial-example-from-scratch/
 
-except that 1) I used Semantic UI instead of Bootstrap and 2) I used a MongoDB database hosted on mLab.com instead of hosting the data locally.
+except that 
+1) I used Semantic UI instead of Bootstrap and 
+2) I used a MongoDB database hosted on mLab.com instead of hosting the data locally.
+
+10 Feb - Attempting to deploy app to Heroku.
+
--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
-const PORT = 4000;
+const port = process.env.PORT || 4000;
 const cors = require("cors");
-const config = require("./DB");
+require("dotenv").config();
 const ServerPortRouter = require("./routes/ServerPortRouter");
 
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(() => {
@@ -18,12 +19,17 @@ mongoose.connect(config.DB, { useNewUrlParser: true }).then(() => {
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.use("/serverport", ServerPortRouter);
 
 app.get("/test", (req, res) => res.send("Here I am at the test endpoint."));
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(PORT, () => {
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(port, () => {
   console.log("Server running on the following port", PORT);
 });
